1966a3864cc47496eda459af9d9518797065427a
anmol1104 <anmpahwa@ucdavis.edu>
2022-11-28 15:40:21 -0800

    update geninstance

--- src/geninstance.jl
+++ src/geninstance.jl
@@ -11,17 +11,17 @@ function genstr(instance::String, share::Float64)
 end
 
 # Generate instance for tactical decision-making
-function gentac(rng::AbstractRNG, instance::String, share::Float64, consolidation::Int64, dynamism::Float64, day::Int64, strategic::LRP.Solution)
+function gentac(rng::AbstractRNG, instance::String, version::String, share::Float64, consolidation::Int64, dynamism::Float64, day::Int64, strategic::LRP.Solution)
     p = share
     θ = consolidation
     δ = dynamism
     h = day
     s = strategic
     
-    mkpath("instances/$instance/#2. tactical/day $h")
+    mkpath("instances/$instance/#2. tactical - $version/day $h")
 
+    Cᶜ = DataFrame(in = Int64[], x = Float64[], y = Float64[], q = Float64[], te = Float64[], tl = Float64[])
     Cˢ = DataFrame(in = Int64[], x = Float64[], y = Float64[], q = Float64[], te = Float64[], tl = Float64[])
-    Cᵈ = DataFrame(in = Int64[], x = Float64[], y = Float64[], q = Int64[], te = Float64[], tl = Float64[], tr = Float64[])
     Dˢ = DataFrame(in = Int64[], jn = Int64[], x = Float64[], y = Float64[], q = Float64[], pl = Float64[], pu = Float64[], ts = Float64[], te = Float64[], co = Float64[], cf = Float64[])
     Vˢ = DataFrame(iv = Int64[], jv = Int64[], id = Int64[], q = Float64[], l = Float64[], s = Float64[], tf = Float64[], td = Float64[], tc = Float64[], tw = Float64[], r = Int64[], co = Float64[], cf = Float64[])
     
@@ -46,13 +46,20 @@ function gentac(rng::AbstractRNG, instance::String, share::Float64, consolidatio
         Iᶜ = sample(rng, Z[iᵗ], zᶜ, replace=false)
         Iˢ = sample(rng, Iᶜ, zˢ, replace=false)
         Iᵈ = filter(x -> x ∉ Iˢ, Iᶜ)
-        for iˢ ∈ Iˢ push!(Cˢ, (df[iˢ,1], df[iˢ,2], df[iˢ,3], θ, df[iˢ,4], df[iˢ,5])) end
-        for iᵈ ∈ Iᵈ push!(Cᵈ, (df[iᵈ,1], df[iᵈ,2], df[iᵈ,3], θ, df[iᵈ,4], df[iᵈ,5], rand(rng, Uniform(tˢ, tˢ + (tᵉ - tˢ)/2)))) end
+        for iˢ ∈ Iˢ 
+            push!(Cᶜ, (df[iˢ,1], df[iˢ,2], df[iˢ,3], θ, df[iˢ,4], df[iˢ,5])) 
+            push!(Cˢ, (df[iˢ,1], df[iˢ,2], df[iˢ,3], θ, df[iˢ,4], df[iˢ,5])) 
+        end
+        for iᵈ ∈ Iᵈ 
+            rand(rng, Uniform(tˢ, tˢ + (tᵉ - tˢ)/2))
+            push!(Cᶜ, (df[iᵈ,1], df[iᵈ,2], df[iᵈ,3], θ, df[iᵈ,4], df[iᵈ,5]))
+        end
         nᶜ += zᶜ
         nˢ += zˢ
         nᵈ += zᵈ
         δʰ  = min(1.0, max(0.0, (δ * p / θ * n - nᵈ)/(n * p / θ - nᶜ)))
     end
+    for n ∈ 1:nᶜ Cᶜ[n,1] = n + Δn end
     for n ∈ 1:nˢ Cˢ[n,1] = n + Δn end
     
     iᵈ = 0
@@ -63,9 +70,10 @@ function gentac(rng::AbstractRNG, instance::String, share::Float64, consolidatio
         for v ∈ d.V push!(Vˢ, (v.iᵛ, v.jᵛ, iᵈ, v.q, v.l, v.s, v.τᶠ, v.τᵈ, v.τᶜ, v.τʷ, v.r̅, v.πᵒ, v.πᶠ)) end
     end
     
-    CSV.write("instances/$instance/#2. tactical/day $h/customer_nodes.csv", Cˢ)
-    CSV.write("instances/$instance/#2. tactical/day $h/depot_nodes.csv", Dˢ)
-    CSV.write("instances/$instance/#2. tactical/day $h/vehicles.csv", Vˢ)
+    Cᵒ = isequal(version, "counterfactual") ? Cᶜ : Cˢ
+    CSV.write("instances/$instance/#2. tactical - $version/day $h/customer_nodes.csv", Cᵒ)
+    CSV.write("instances/$instance/#2. tactical - $version/day $h/depot_nodes.csv", Dˢ)
+    CSV.write("instances/$instance/#2. tactical - $version/day $h/vehicles.csv", Vˢ)
     return
 end
 
@@ -84,10 +92,9 @@ function genopt(rng::AbstractRNG, instance::String, share::Float64, consolidatio
     for d ∈ s.D if d.tˢ < tˢ tˢ = d.tˢ end end
     for d ∈ s.D if d.tᵉ > tᵉ tᵉ = d.tᵉ end end
     
-    Cˢ = DataFrame(in = Int64[], x = Float64[], y = Float64[], q = Float64[], te = Float64[], tl = Float64[])
-    Cᵈ = DataFrame(in = Int64[], x = Float64[], y = Float64[], q = Int64[], te = Float64[], tl = Float64[], tr = Float64[])
+    Cᵈ = DataFrame(in = Int64[], x = Float64[], y = Float64[], q = Float64[], te = Float64[], tl = Float64[], tr = Float64[])
     Dᵈ = DataFrame(in = Int64[], jn = Int64[], x = Float64[], y = Float64[], q = Int64[], pl = Float64[], pu = Float64[], ts = Int64[], te = Int64[], co = Float64[], cf = Float64[])
-    Vᵈ = DataFrame(iv = Int64[], jv = Int64[], id = Int64[], q = Int64[], l = Int64[], s = Int64[], tf = Float64[], td = Float64[], tc = Float64[], tw = Int64[], r = Int64[], co = Float64[], cf = Float64[])
+    Vᵈ = DataFrame(iv = Int64[], jv = Int64[], id = Int64[], q = Float64[], l = Float64[], s = Float64[], tf = Float64[], td = Float64[], tc = Float64[], tw = Float64[], r = Int64[], co = Float64[], cf = Float64[])
 
     df = DataFrame(CSV.File("instances/$instance/customer nodes.csv"))
     n  = nrow(df)
@@ -105,7 +112,6 @@ function genopt(rng::AbstractRNG, instance::String, share::Float64, consolidatio
         Iᶜ = sample(rng, Z[iᵗ], zᶜ, replace=false)
         Iˢ = sample(rng, Iᶜ, zˢ, replace=false)
         Iᵈ = filter(x -> x ∉ Iˢ, Iᶜ)
-        for iˢ ∈ Iˢ push!(Cˢ, (df[iˢ,1], df[iˢ,2], df[iˢ,3], θ, df[iˢ,4], df[iˢ,5])) end
         for iᵈ ∈ Iᵈ push!(Cᵈ, (df[iᵈ,1], df[iᵈ,2], df[iᵈ,3], θ, df[iᵈ,4], df[iᵈ,5], rand(rng, Uniform(tˢ, tˢ + (tᵉ - tˢ)/2)))) end
         nᶜ += zᶜ
         nˢ += zˢ
