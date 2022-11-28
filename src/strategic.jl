879d663a37d1597f4db8fa32db5c74e1517d1496
anmol1104 <anmpahwa@ucdavis.edu>
2022-11-28 15:43:29 -0800

    add ALNS optimzation for strategic decision-making

--- src/strategic.jl
+++ src/strategic.jl
@@ -9,6 +9,57 @@ function optstr(rng::AbstractRNG, instance::String)
     G  = build(instance; root=dir)
     s  = initialsolution(rng, G, :cluster)
 
+    n  = 200
+    χ  = ALNSParameters(
+        n    =   n ÷ 25                  ,
+        k    =   250                     ,
+        m    =   2n                      ,
+        j    =   125                     ,
+        Ψᵣ   =   [
+                    :randomnode!    ,
+                    :randomroute!   ,
+                    :randomvehicle! ,
+                    :randomdepot!   ,
+                    :relatednode!   ,
+                    :relatedroute!  ,
+                    :relatedvehicle!,
+                    :relateddepot!  ,
+                    :worstnode!     ,
+                    :worstroute!    ,
+                    :worstvehicle!  ,
+                    :worstdepot!
+                ]                        ,
+        Ψᵢ   =  [
+                    :bestprecise!   ,
+                    :bestperturb!   ,
+                    :greedyprecise! ,
+                    :greedyperturb! ,
+                    :regret2!       ,
+                    :regret3!
+                ]                        ,
+        Ψₗ  =   [
+                    :intraopt!       ,
+                    :interopt!       ,
+                    :movecustomer!   ,
+                    :movedepot!      ,
+                    :swapcustomers!  ,
+                    :swapdepots!
+                ]                        ,
+        σ₁   =   15                      ,
+        σ₂   =   10                      ,
+        σ₃   =   3                       ,
+        ω    =   0.05                    ,
+        τ    =   0.5                     ,
+        𝜃    =   0.9975                  ,
+        C̲    =   4                       ,
+        C̅    =   60                      ,
+        μ̲    =   0.1                     ,
+        μ̅    =   0.4                     ,
+        ρ    =   0.1
+    )
+    S = ALNS(rng, χ, s)
+    s = deepcopy(S[end])
+
     # Step 3. Return solution
     return s
 end
