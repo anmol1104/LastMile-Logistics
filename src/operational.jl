08c5a656fcc0077e0374280db119e145f3c066ca
anmol1104 <anmpahwa@ucdavis.edu>
2022-12-31 17:43:36 -0800

    update operational decision-making

--- src/operational.jl
+++ src/operational.jl
@@ -5,7 +5,6 @@ include("localsearch.jl")
 function optopr(rng::AbstractRNG, instance::String, day::Int64, tactical::LRP.Solution)
     instance = "$instance/#3. operational/day $day"
     dir      = "G:/My Drive/Academia/Research/Projects/2022. Last-Mile Logistics/Analysis/instances"
-
     file     = joinpath(dir, "$instance/customer_nodes.csv")
     csv      = CSV.File(file, types=[Int64, Float64, Float64, Float64, Float64, Float64, Float64])
     df       = DataFrame(csv)
@@ -28,52 +27,39 @@ function optopr(rng::AbstractRNG, instance::String, day::Int64, tactical::LRP.So
         C′[iⁿ] = c
         Tʳ[iⁿ] = tʳ
     end
-
-    file     = joinpath(dir, "$instance/arcs.csv")
-    csv      = CSV.File(file, types=[Int64, Int64, Float64, Float64])
-    df       = DataFrame(csv)
-    A′       = Dict{Tuple{Int64,Int64},LRP.Arc}()
-    for k ∈ 1:nrow(df)
-        iⁿ = df[k,1]::Int64
-        jⁿ = df[k,2]::Int64
-        l  = df[k,3]::Float64
-        φ  = df[k,4]::Float64
-        a  = LRP.Arc(iⁿ, jⁿ, l, φ)
-        A′[(iⁿ,jⁿ)] = a
-    end
-
     # Step 1. Simulate daily operations for day-t
     s = deepcopy(tactical)
-    D = s.D
-    C = s.C
-    A = s.A
-    N = length(D) + length(C)
+    N = length(s.D) + length(s.C)
     tˢ = Inf
     tᵉ = 0.
-    for d ∈ D if d.tˢ < tˢ tˢ = d.tˢ end end
-    for d ∈ D if d.tᵉ > tᵉ tᵉ = d.tᵉ end end
+    for d ∈ s.D if d.tˢ < tˢ tˢ = d.tˢ end end
+    for d ∈ s.D if d.tᵉ > tᵉ tᵉ = d.tᵉ end end
     τ = 1.
     t = tˢ - τ
     p = Progress(Int64((tᵉ - tˢ)/τ), desc="Computing...", color=:blue, showspeed=true)
     while t ≤ tᵉ
-        # Step 1.1. Process delivery requests
+        # Step 1.1. Process delivery commitments
         for c ∈ C′
             iⁿ = c.iⁿ
             tʳ = Tʳ[iⁿ]
             tᶜ = tˢ + ceil((tʳ - tˢ)/τ) * τ
             if isequal(tᶜ, t)
-                push!(C, c)
+                push!(s.C, c)
                 N += 1
                 for jⁿ ∈ 1:N
-                    A[(iⁿ,jⁿ)] = A′[(iⁿ, jⁿ)]
-                    A[(jⁿ,iⁿ)] = A′[(jⁿ, iⁿ)]
+                    n = jⁿ ≤ length(s.D) ? s.D[jⁿ] : s.C[jⁿ]
+                    x¹, y¹ = c.x, c.y
+                    x², y² = n.x, n.y
+                    l = sqrt((x² - x¹)^2 + (y² - y¹)^2)
+                    s.A[(iⁿ,jⁿ)] = LRP.Arc(iⁿ, jⁿ, l)
+                    s.A[(jⁿ,iⁿ)] = LRP.Arc(jⁿ, iⁿ, l)
                 end
+                insertnode!(rng, s)
             end
         end
-        insertnode!(rng, s)
-        localsearch!(rng, 1000, s)
+        localsearch!(rng, 5000, s)
         # Step 1.2. Process route commitments
-        for d ∈ D
+        for d ∈ s.D
             for v ∈ d.V
                 for r ∈ v.R
                     if !LRP.isopt(r) continue end
@@ -83,20 +69,18 @@ function optopr(rng::AbstractRNG, instance::String, day::Int64, tactical::LRP.So
                         r.φ  = 0 
                         r.tⁱ = tᶜ
                         r.tˢ = r.tⁱ + v.τᶠ * (r.θˢ - r.θⁱ) + v.τᵈ * r.q
-                        cˢ = C[r.iˢ]
-                        cᵉ = C[r.iᵉ]
+                        cˢ = s.C[r.iˢ]
+                        cᵉ = s.C[r.iᵉ]
                         tᵈ = r.tˢ
                         cᵒ = cˢ
                         while true
-                            a = A[(cᵒ.iᵗ,cᵒ.iⁿ)]
-                            cᵒ.tᵃ = tᵈ + a.l/(v.s * a.φ)
+                            cᵒ.tᵃ = tᵈ + s.A[(cᵒ.iᵗ,cᵒ.iⁿ)].l/v.s
                             cᵒ.tᵈ = cᵒ.tᵃ + max(0., cᵒ.tᵉ - cᵒ.tᵃ) + v.τᶜ
                             if isequal(cᵒ, cᵉ) break end
                             tᵈ = cᵒ.tᵈ
-                            cᵒ = C[cᵒ.iʰ]
+                            cᵒ = s.C[cᵒ.iʰ]
                         end
-                        a = A[(cᵉ.iⁿ, cᵉ.iʰ)]
-                        r.tᵉ = cᵉ.tᵈ + a.l/(v.s * a.φ)
+                        r.tᵉ = cᵉ.tᵈ + s.A[(cᵉ.iⁿ, cᵉ.iʰ)].l/v.s
                     end
                 end
                 (v.tˢ, v.tᵉ) = isempty(v.R) ? (d.tˢ, d.tˢ) : (v.R[1].tⁱ, v.R[length(v.R)].tᵉ)
@@ -106,18 +90,18 @@ function optopr(rng::AbstractRNG, instance::String, day::Int64, tactical::LRP.So
         next!(p)
     end
     # Step 1.3. Update route slack times
-    for d ∈ D
+    for d ∈ s.D
         for v ∈ d.V
             τ = d.tᵉ - v.tᵉ
             for r ∈ reverse(v.R)
                 if !LRP.isopt(r) continue end
-                cˢ = C[r.iˢ]
-                cᵉ = C[r.iᵉ]
+                cˢ = s.C[r.iˢ]
+                cᵉ = s.C[r.iᵉ]
                 cᵒ = cˢ
                 while true
                     τ  = min(τ, cᵒ.tˡ - cᵒ.tᵃ)
                     if isequal(cᵒ, cᵉ) break end
-                    cᵒ = C[cᵒ.iʰ]
+                    cᵒ = s.C[cᵒ.iʰ]
                 end
                 r.τ = τ
             end
