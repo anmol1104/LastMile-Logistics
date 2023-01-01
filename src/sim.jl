c7fa18bd3a7ef384608bea27b69253d43fcdac0e
anmol1104 <anmpahwa@ucdavis.edu>
2022-12-31 17:43:45 -0800

    update sim.jl

--- src/sim.jl
+++ src/sim.jl
@@ -16,8 +16,6 @@ include("tactical.jl");
 include("operational.jl");
 include("geninstance.jl");
 
-isopt = LRP.isopt;
-
 let
 ##
     df = DataFrame(
@@ -38,17 +36,18 @@ let
 ##
     instance      = "LosAngeles/#3. MH"
     horizon       = 30          
-    share         = 0.007695    
+    share         = 0.01    
     consolidation = 3           
     dynamism      = 0.2
     x₀            = 60.0
     y₀            = 26.0
+    isopt         = LRP.isopt
 ##;
 
 
 ##  Step 1. Strategic decision-making 
     println("STRATEGIC DECISION-MAKING")
-    strategic = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0, 0) 
+    strategic = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0) 
     solution = "instances/$instance/#1. strategic/solution.jld"
     try strategic = load(solution)["s"]
     catch
@@ -70,7 +69,7 @@ let
         k  = d.iⁿ
         x₁ = d.x
         y₁ = d.y
-        ft₁ += abs(x₁-x₀) + abs(y₁-y₀)
+        ft₁ += 2(abs(x₁-x₀) + abs(y₁-y₀))
         for v ∈ d.V for r ∈ v.R dt₁ += r.l end end
     end
     for d ∈ s.D
@@ -81,21 +80,21 @@ let
         y₁ = d₁.y
         x₂ = d.x
         y₂ = d.y
-        ft₂ += abs(x₂-x₁) + abs(y₂-y₁)
+        ft₂ += 2(abs(x₂-x₁) + abs(y₂-y₁))
         for v ∈ d.V for r ∈ v.R dt₂ += r.l end end
     end
     push!(df, (0, "str", length(s.C), ft₁, ft₂, dt₁, dt₂, f(s; fixed=true, operational=false, penalty=false), f(s; fixed=false, operational=true, penalty=false), isfeasible(s)))
 ##;
 
 
-##  Simulate daily operations
+##  Step 2. Simulate daily operations
     for day ∈ 1:horizon
         println("\nday $day")
 
         # Step 2.1. Tactical decision-making (counterfactual)
         println("TACTICAL DECISION-MAKING - COUNTERFACTUAL")
         version  = "counterfactual" 
-        tactical = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0, 0) 
+        tactical = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0) 
         solution = "instances/$instance/#2. tactical - $version/day $day/solution.jld"
         try tactical = load(solution)["s"] 
         catch
@@ -117,7 +116,7 @@ let
             k  = d.iⁿ
             x₁ = d.x
             y₁ = d.y
-            ft₁ += abs(x₁-x₀) + abs(y₁-y₀)
+            ft₁ += 2(abs(x₁-x₀) + abs(y₁-y₀))
             for v ∈ d.V for r ∈ v.R dt₁ += r.l end end
         end
         for d ∈ s.D
@@ -128,7 +127,7 @@ let
             y₁ = d₁.y
             x₂ = d.x
             y₂ = d.y
-            ft₂ += abs(x₂-x₁) + abs(y₂-y₁)
+            ft₂ += 2(abs(x₂-x₁) + abs(y₂-y₁))
             for v ∈ d.V for r ∈ v.R dt₂ += r.l end end
         end
         push!(df, (day, "tac-cft", length(s.C), ft₁, ft₂, dt₁, dt₂, f(s; fixed=true, operational=false, penalty=false), f(s; fixed=false, operational=true, penalty=false), isfeasible(s)))
@@ -136,7 +135,7 @@ let
         # Step 2.2. Tactical decision-making (actual)
         println("TACTICAL DECISION-MAKING - ACTUAL")
         version  = "actual" 
-        tactical = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0, 0) 
+        tactical = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0) 
         solution = "instances/$instance/#2. tactical - $version/day $day/solution.jld"
         try tactical = load(solution)["s"] 
         catch
@@ -158,7 +157,7 @@ let
             k  = d.iⁿ
             x₁ = d.x
             y₁ = d.y
-            ft₁ += abs(x₁-x₀) + abs(y₁-y₀)
+            ft₁ += 2(abs(x₁-x₀) + abs(y₁-y₀))
             for v ∈ d.V for r ∈ v.R dt₁ += r.l end end
         end
         for d ∈ s.D
@@ -169,14 +168,14 @@ let
             y₁ = d₁.y
             x₂ = d.x
             y₂ = d.y
-            ft₂ += abs(x₂-x₁) + abs(y₂-y₁)
+            ft₂ += 2(abs(x₂-x₁) + abs(y₂-y₁))
             for v ∈ d.V for r ∈ v.R dt₂ += r.l end end
         end
         push!(df, (day, "tac-act", length(s.C), ft₁, ft₂, dt₁, dt₂, f(s; fixed=true, operational=false, penalty=false), f(s; fixed=false, operational=true, penalty=false), isfeasible(s)))
         
         # Step 2.3. Operational decision-making
         println("OPERATIONAL DECISION-MAKING")
-        operational = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0, 0) 
+        operational = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0) 
         solution = "instances/$instance/#3. operational/day $day/solution.jld"
         try operational = load(solution)["s"] 
         catch
@@ -198,7 +197,7 @@ let
             k  = d.iⁿ
             x₁ = d.x
             y₁ = d.y
-            ft₁ += abs(x₁-x₀) + abs(y₁-y₀)
+            ft₁ += 2(abs(x₁-x₀) + abs(y₁-y₀))
             for v ∈ d.V for r ∈ v.R dt₁ += r.l end end
         end
         for d ∈ s.D
@@ -209,12 +208,15 @@ let
             y₁ = d₁.y
             x₂ = d.x
             y₂ = d.y
-            ft₂ += abs(x₂-x₁) + abs(y₂-y₁)
+            ft₂ += 2(abs(x₂-x₁) + abs(y₂-y₁))
             for v ∈ d.V for r ∈ v.R dt₂ += r.l end end
         end
         push!(df, (day, "opt", length(s.C), ft₁, ft₂, dt₁, dt₂, f(s; fixed=true, operational=false, penalty=false), f(s; fixed=false, operational=true, penalty=false), isfeasible(s)))  
+        println(df[(end-2:end), :])
     end
 ##;
+
+
 ##
     CSV.write("instances/$instance/analysis.csv", df)
 ##;
