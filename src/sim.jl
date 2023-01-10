3a464578635241e73db3308b6415f4a19a2b3dcd
anmol1104 <anmpahwa@ucdavis.edu>
2023-01-09 19:22:11 -0800

    update sim.jl

--- src/sim.jl
+++ src/sim.jl
@@ -17,44 +17,57 @@ include("operational.jl");
 include("geninstance.jl");
 
 let
-##
-    df = DataFrame(
-            day               = Int64[]  , 
-            decision          = String[] , 
-            customers         = Int64[]  ,
-            fulfillmentTripE1 = Float64[], 
-            fulfillmentTripE2 = Float64[], 
-            deliveryTourE1    = Float64[], 
-            deliveryTourE2    = Float64[],
-            fixed             = Float64[],
-            operational       = Float64[],
-            feasible          = Bool[]
-        )                          
-##;
-
 
 ##
-    instance      = "LosAngeles/#3. MH"
-    horizon       = 30          
-    share         = 0.01    
-    consolidation = 3           
+    dir           = "G:/My Drive/Academia/Research/Projects/2022. Last-Mile Logistics/Analysis/instances"
+    strategy      = "LosAngeles/#5. DDMHCP"
+    horizon       = 30
+    share         = 0.01
+    consolidation = 3
     dynamism      = 0.2
-    x₀            = 60.0
-    y₀            = 26.0
-    isopt         = LRP.isopt
+    x₀            = 50.0
+    y₀            = 0.00
+    day           = 0
+    structure     = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0)
+
+    env = Dict()
+    env["dir"]           = dir
+    env["strategy"]      = strategy
+    env["horizon"]       = horizon
+    env["share"]         = share
+    env["consolidation"] = consolidation
+    env["dynamism"]      = dynamism
+    env["x₀"]            = x₀
+    env["y₀"]            = y₀
+    env["day"]           = day
+    env["structure"]     = structure
+
+    df = DataFrame(
+        day               = Int64[]  , 
+        decision          = String[] , 
+        customers         = Int64[]  ,
+        fulfillmentTripE1 = Float64[],
+        fulfillmentTripE2 = Float64[], 
+        deliveryTourE1    = Float64[], 
+        deliveryTourE2    = Float64[],
+        fixed             = Float64[],
+        operational       = Float64[],
+        feasible          = Bool[]
+    )                          
 ##;
 
 
 ##  Step 1. Strategic decision-making 
     println("STRATEGIC DECISION-MAKING")
-    strategic = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0) 
-    solution = "instances/$instance/#1. strategic/solution.jld"
-    try strategic = load(solution)["s"]
+    path          = joinpath(dir, "$strategy/#1. strategic")
+    strategic     = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0) 
+    try strategic = load("$path/solution.jld")["s"]
     catch
-        rng = MersenneTwister(horizon)
-        genstr(instance, share)
-        strategic = optstr(rng, instance)
-        save(solution, "s", strategic)
+        rng       = MersenneTwister(horizon)
+        genstr(rng, env, path)
+        initsol   = initialsolution(rng, build(path; root=dir), :cluster)
+        strategic = optstr(rng, env, initsol)
+        save("$path/solution.jld", "s", strategic)
     end
     s = deepcopy(strategic)
     display(visualize(s))
@@ -64,7 +77,7 @@ let
     dt₁ = 0.
     dt₂ = 0.
     for d ∈ s.D
-        if !isopt(d) continue end
+        if !LRP.isopt(d) continue end
         if !isone(d.jⁿ) continue end
         k  = d.iⁿ
         x₁ = d.x
@@ -73,7 +86,7 @@ let
         for v ∈ d.V for r ∈ v.R dt₁ += r.l end end
     end
     for d ∈ s.D
-        if !isopt(d) continue end
+        if !LRP.isopt(d) continue end
         if isone(d.jⁿ) continue end
         d₁ = s.D[k]
         x₁ = d₁.x
@@ -84,115 +97,118 @@ let
         for v ∈ d.V for r ∈ v.R dt₂ += r.l end end
     end
     push!(df, (0, "str", length(s.C), ft₁, ft₂, dt₁, dt₂, f(s; fixed=true, operational=false, penalty=false), f(s; fixed=false, operational=true, penalty=false), isfeasible(s)))
+    env["structure"] = deepcopy(strategic)
 ##;
 
 
 ##  Step 2. Simulate daily operations
     for day ∈ 1:horizon
         println("\nday $day")
+        env["day"] = day
 
-        # Step 2.1. Tactical decision-making (counterfactual)
-        println("TACTICAL DECISION-MAKING - COUNTERFACTUAL")
-        version  = "counterfactual" 
-        tactical = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0) 
-        solution = "instances/$instance/#2. tactical - $version/day $day/solution.jld"
-        try tactical = load(solution)["s"] 
-        catch
-            rng = MersenneTwister(day)
-            gentac(rng, instance, version, share, consolidation, dynamism, day, strategic)
-            tactical = opttac(rng, instance, version, day)
-            save(solution, "s", tactical)
-        end
-        s = deepcopy(tactical)
-        display(visualize(s))
-        k   = 0
-        ft₁ = 0.
-        ft₂ = 0.
-        dt₁ = 0.
-        dt₂ = 0.
-        for d ∈ s.D
-            if !isopt(d) continue end
-            if !isone(d.jⁿ) continue end
-            k  = d.iⁿ
-            x₁ = d.x
-            y₁ = d.y
-            ft₁ += 2(abs(x₁-x₀) + abs(y₁-y₀))
-            for v ∈ d.V for r ∈ v.R dt₁ += r.l end end
-        end
-        for d ∈ s.D
-            if !isopt(d) continue end
-            if isone(d.jⁿ) continue end
-            d₁ = s.D[k]
-            x₁ = d₁.x
-            y₁ = d₁.y
-            x₂ = d.x
-            y₂ = d.y
-            ft₂ += 2(abs(x₂-x₁) + abs(y₂-y₁))
-            for v ∈ d.V for r ∈ v.R dt₂ += r.l end end
-        end
-        push!(df, (day, "tac-cft", length(s.C), ft₁, ft₂, dt₁, dt₂, f(s; fixed=true, operational=false, penalty=false), f(s; fixed=false, operational=true, penalty=false), isfeasible(s)))
-
-        # Step 2.2. Tactical decision-making (actual)
+        # Step 2.1. Tactical decision-making (actual)
         println("TACTICAL DECISION-MAKING - ACTUAL")
-        version  = "actual" 
-        tactical = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0) 
-        solution = "instances/$instance/#2. tactical - $version/day $day/solution.jld"
-        try tactical = load(solution)["s"] 
+        path        = joinpath(dir, "$strategy/#2. tactical - actual/day $day")
+        tacact      = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0)         
+        try tacact  = load("$path/solution.jld")["s"] 
         catch
-            rng = MersenneTwister(day)
-            gentac(rng, instance, version, share, consolidation, dynamism, day, strategic)
-            tactical = opttac(rng, instance, version, day)
-            save(solution, "s", tactical)
+            rng     = MersenneTwister(day)
+            genact(rng, env, path)
+            initsol = initialsolution(rng, build(path; root=dir), :cluster)
+            tacact  = optact(rng, env, initsol)
+            save("$path/solution.jld", "s", tacact)
         end
-        s = deepcopy(tactical)
-        display(visualize(s))
+        display(visualize(tacact))
+        s   = deepcopy(tacact)
         k   = 0
         ft₁ = 0.
         ft₂ = 0.
         dt₁ = 0.
         dt₂ = 0.
         for d ∈ s.D
-            if !isopt(d) continue end
+            if !LRP.isopt(d) continue end
             if !isone(d.jⁿ) continue end
-            k  = d.iⁿ
-            x₁ = d.x
-            y₁ = d.y
-            ft₁ += 2(abs(x₁-x₀) + abs(y₁-y₀))
+            k   = d.iⁿ
+            x₁  = d.x
+            y₁  = d.y
+            ft₁+= 2(abs(x₁-x₀) + abs(y₁-y₀))
             for v ∈ d.V for r ∈ v.R dt₁ += r.l end end
         end
         for d ∈ s.D
-            if !isopt(d) continue end
+            if !LRP.isopt(d) continue end
             if isone(d.jⁿ) continue end
-            d₁ = s.D[k]
-            x₁ = d₁.x
-            y₁ = d₁.y
-            x₂ = d.x
-            y₂ = d.y
-            ft₂ += 2(abs(x₂-x₁) + abs(y₂-y₁))
+            d₁  = s.D[k]
+            x₁  = d₁.x
+            y₁  = d₁.y
+            x₂  = d.x
+            y₂  = d.y
+            ft₂+= 2(abs(x₂-x₁) + abs(y₂-y₁))
             for v ∈ d.V for r ∈ v.R dt₂ += r.l end end
         end
         push!(df, (day, "tac-act", length(s.C), ft₁, ft₂, dt₁, dt₂, f(s; fixed=true, operational=false, penalty=false), f(s; fixed=false, operational=true, penalty=false), isfeasible(s)))
-        
-        # Step 2.3. Operational decision-making
-        println("OPERATIONAL DECISION-MAKING")
-        operational = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0) 
-        solution = "instances/$instance/#3. operational/day $day/solution.jld"
-        try operational = load(solution)["s"] 
+
+        # Step 2.2. Tactical decision-making (counterfactual)
+        println("TACTICAL DECISION-MAKING - COUNTERFACTUAL")
+        path        = joinpath(dir, "$strategy/#2. tactical - counterfactual/day $day")
+        taccft      = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0) 
+        try taccft  = load("$path/solution.jld")["s"] 
         catch
-            rng = MersenneTwister(day)
-            genopt(rng, instance, share, consolidation, dynamism, day, strategic)
-            operational = optopr(rng, instance, day, tactical)
-            save(solution, "s", operational)
+            rng     = MersenneTwister(day)
+            gencft(rng, env, path)
+            initsol = deepcopy(tacact)
+            taccft  = optcft(rng, env, initsol)
+            save("$path/solution.jld", "s", taccft)
         end
-        s = deepcopy(operational)
-        display(visualize(s))
+        display(visualize(taccft))
+        s   = deepcopy(taccft)
         k   = 0
         ft₁ = 0.
         ft₂ = 0.
         dt₁ = 0.
         dt₂ = 0.
         for d ∈ s.D
-            if !isopt(d) continue end
+            if !LRP.isopt(d) continue end
+            if !isone(d.jⁿ) continue end
+            k  = d.iⁿ
+            x₁ = d.x
+            y₁ = d.y
+            ft₁+= 2(abs(x₁-x₀) + abs(y₁-y₀))
+            for v ∈ d.V for r ∈ v.R dt₁ += r.l end end
+        end
+        for d ∈ s.D
+            if !LRP.isopt(d) continue end
+            if isone(d.jⁿ) continue end
+            d₁  = s.D[k]
+            x₁  = d₁.x
+            y₁  = d₁.y
+            x₂  = d.x
+            y₂  = d.y
+            ft₂+= 2(abs(x₂-x₁) + abs(y₂-y₁))
+            for v ∈ d.V for r ∈ v.R dt₂ += r.l end end
+        end
+        push!(df, (day, "tac-cft", length(s.C), ft₁, ft₂, dt₁, dt₂, f(s; fixed=true, operational=false, penalty=false), f(s; fixed=false, operational=true, penalty=false), isfeasible(s)))
+        
+        # Step 2.3. Operational decision-making
+        println("OPERATIONAL DECISION-MAKING")
+        path        = joinpath(dir, "$strategy/#3. operational/day $day")
+        operational = LRP.Solution(LRP.DepotNode[], LRP.CustomerNode[], Dict{Tuple{Int64, Int64}, LRP.Arc}(), 0) 
+        try operational = load("$path/solution.jld")["s"] 
+        catch
+            rng         = MersenneTwister(day)
+            genopt(rng, env, path)
+            initsol     = deepcopy(tacact)
+            operational = optopr(rng, env, initsol)
+            save("$path/solution.jld", "s", operational)
+        end
+        display(visualize(operational))
+        s   = deepcopy(operational)
+        k   = 0
+        ft₁ = 0.
+        ft₂ = 0.
+        dt₁ = 0.
+        dt₂ = 0.
+        for d ∈ s.D
+            if !LRP.isopt(d) continue end
             if !isone(d.jⁿ) continue end
             k  = d.iⁿ
             x₁ = d.x
@@ -201,7 +217,7 @@ let
             for v ∈ d.V for r ∈ v.R dt₁ += r.l end end
         end
         for d ∈ s.D
-            if !isopt(d) continue end
+            if !LRP.isopt(d) continue end
             if isone(d.jⁿ) continue end
             d₁ = s.D[k]
             x₁ = d₁.x
@@ -216,8 +232,6 @@ let
     end
 ##;
 
+CSV.write("instances/$strategy/analysis.csv", df)
 
-##
-    CSV.write("instances/$instance/analysis.csv", df)
-##;
 end
